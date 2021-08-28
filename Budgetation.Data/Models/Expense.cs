66bc7b271ae0c7afd1b2325662438e539df164cb
Tommy Kovalchuk <tommy.pk17@gmail.com>
2021-08-28 18:15:26 -0400

    Fix expense type display

--- Budgetation.Data/Models/Expense.cs
+++ Budgetation.Data/Models/Expense.cs
@@ -5,6 +5,6 @@ namespace Budgetation.Data.Models
     {
         public string Name { get; set; }
         public double Amount { get; set; }
-        public string ExpenseType { get; set; }
+        public string Type { get; set; }
     }
 }
