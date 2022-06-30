5a492a5930ea35cf108a29b63477fd826a1bdda7
Tommy Kovalchuk <tommy.pk17@gmail.com>
2022-06-30 18:42:33 -0400

    mark paid functionality

--- Budgetation.App/ClientApp/src/app/modules/expenses/components/atoms/existing-expense-block/existing-expense-block.component.ts
+++ Budgetation.App/ClientApp/src/app/modules/expenses/components/atoms/existing-expense-block/existing-expense-block.component.ts
@@ -13,6 +13,7 @@ export class ExistingExpenseBlockComponent implements OnInit {
   @Input('bill') bill: iBill | undefined;
   @Output('save') save: Subject<iBill> = new Subject<iBill>();
   @Output('delete') delete: Subject<iBill> = new Subject<iBill>();
+  @Output('paid') paid: Subject<iBill> = new Subject<iBill>();
 
   edit: boolean = false;
 
@@ -35,5 +36,9 @@ export class ExistingExpenseBlockComponent implements OnInit {
 
   deleteBill(bill: iBill): void {
     this.delete.next(bill);
+  }
+
+  markPaid(bill: iBill): void {
+    this.paid.next(bill)
   }
 }
