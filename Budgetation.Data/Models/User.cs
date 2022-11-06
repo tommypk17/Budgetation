b6c8aa6efad468cb809f259a5ec9a7ec63e662bc
Tommy Kovalchuk <tommy-pk17@outlook.com>
2022-11-06 18:53:26 -0500

    remove unused model

--- Budgetation.Data/Models/User.cs
+++ Budgetation.Data/Models/User.cs
@@ -16,15 +16,7 @@ namespace Budgetation.Data.Models
         public List<Role> Roles { get; set; } = new List<Role>() {new Role()};
         public List<UserPreference> Preferences { get; set; } = new List<UserPreference>();
     }
-
-    public class UserExpense
-    {
-        [BsonId]
-        public Guid UserId { get; set; }
-        public List<SingleExpense> SingleExpenses { get; set; } = new List<SingleExpense>();
-        public List<RecurringExpense> RecurringExpenses { get; set; } = new List<RecurringExpense>();
-    }
-
+    
     public class UserPreference
     {
         [BsonId]
