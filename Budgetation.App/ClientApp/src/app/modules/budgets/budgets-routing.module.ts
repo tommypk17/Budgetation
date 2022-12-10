cdf136903ae3d91ae9679740260d5f6323080042
Tommy Kovalchuk <tommy-pk17@outlook.com>
2022-12-10 06:00:51 -0500

    update styles and routing canDeactivate

--- Budgetation.App/ClientApp/src/app/modules/budgets/budgets-routing.module.ts
+++ Budgetation.App/ClientApp/src/app/modules/budgets/budgets-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {BudgetsComponent} from "./components/budgets/budgets.component";
 import {WhatIfComponent} from "./components/what-if/what-if.component";
 import {BudgetDetailsComponent} from "./components/budget-details/budget-details.component";
+import {PreviousPageGuard} from "../../guards/previous-page.guard";
 
 const routes: Routes = [
-  {path: '', component: BudgetsComponent},
-  {path: 'what-if', component: WhatIfComponent},
-  {path: ':budgetId', component: BudgetDetailsComponent}
+  {path: '', component: BudgetsComponent, canDeactivate: [PreviousPageGuard]},
+  {path: 'what-if', component: WhatIfComponent, canDeactivate: [PreviousPageGuard]},
+  {path: ':budgetId', component: BudgetDetailsComponent, canDeactivate: [PreviousPageGuard]}
 ];
 
 @NgModule({
